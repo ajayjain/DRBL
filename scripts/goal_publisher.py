986adb9113944a02d94483ac0d994de1113e3029
Ajay Jain <ajayjain318@gmail.com>
2014-06-21 15:39:59 -0700

    Documentation, rename launchs, work on goal_publisher

--- scripts/goal_publisher.py
+++ scripts/goal_publisher.py
@@ -4,6 +4,8 @@ Author: Ajay Jain
 Created: 21 June 2014
 '''
 
+# TODO: Monitor goal progress
+
 from __future__ import print_function
 
 import sys
@@ -14,24 +16,39 @@ from actionlib_msgs.msg import GoalID
 from geometry_msgs.msg import PoseStamped, Pose
 
 goal_pub = None
-goal = generate_goal()
+goal = None
 
+# Scaffold empty MoveBaseActionGoal goal
 def generate_goal():
-	pose_stamped = PoseStamped(None)
+	global goal
+
+	pose_stamped = PoseStamped()
+	pose_stamped.pose = None # no goal at start
+
+	move_base_goal = MoveBaseGoal()
+	move_base_goal.target_pose = pose_stamped
+
+	goal = MoveBaseActionGoal()
+	goal.goal = move_base_goal
 
 def on_pose(pose):
-	target_pose = pose
-	goal.goal.target_pose.pose = target_pose
+	print("Received pose", pose)
+	goal.goal.target_pose.pose = pose  # TODO: modify pose? Perhaps some distance behind target
 	goal_pub.publish(goal)
+	print("Published", goal)
 
 def main():
-	global goal_pub, goal
+	global goal_pub
 
 	rospy.init_node("goal_publisher")
 
-	print("Creating move_base/goal publisher")
+	generate_goal()
+
+	print("Creating move_base/goal publisher.")
 	goal_pub = rospy.Publisher("move_base/goal", MoveBaseActionGoal)
-	rospy.Subscriber
+	print("Creating amcl_pose/pose Subscriber.")
+	rospy.Subscriber("amcl_pose/pose", on_pose, Pose) # TODO: enemy pose, not my _own_ pose
+	print("Listening and publishing.")
 	rospy.spin()
 
 if __name__ == "__main__":
