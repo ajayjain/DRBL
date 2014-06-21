5fd9a0ee66de155edf2350a4172151ab9f1c0360
Ajay Jain <ajayjain318@gmail.com>
2014-06-21 11:34:44 -0700

    Scaffolded state_publisher

--- scripts/state_publisher.py
+++ scripts/state_publisher.py
@@ -4,24 +4,31 @@ Author: Ajay Jain
 Created: 21 June 2014
 '''
 
+from __future__ import print_function
+
 import sys
 import rospy
 from std_msgs.msg import String
 
 mode_pub = target_pub = None
 mode = String("idle") # pursuit | evasion | patrol | idle
+target = String("B")
 
 def main():
 	global mode_pub, target_pub
 
 	rospy.init_node("state_publisher")
 
+	print("Creating status/mode and status/target publishers.")
 	mode_pub = rospy.Publisher("status/mode", String)
 	target_pub = rospy.Publisher("status/target", String)
 
-	r = rospy.rate(1)
+	print("Publishing.")
+	r = rospy.Rate(10) # 10 Hz
 	while not rospy.is_shutdown():
-		target_pub.publish
+		mode_pub.publish(mode)
+		target_pub.publish(target)
+		# print("Mode: {}, Target: {}".format(mode.data, target.data))
 		r.sleep()
 
 if __name__ == "__main__":
