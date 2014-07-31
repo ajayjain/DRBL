fe18ea33d6546b061d788e21c38b304169173758
Ajay Jain <ajayjain318@gmail.com>
2014-07-30 17:40:43 -0700

    Add stamped header to pose2d converter, reorg file

--- scripts/pose2d_to_odometry.py
+++ scripts/pose2d_to_odometry.py
@@ -7,28 +7,45 @@ ARSENL Lab, Naval Postgraduate School
 
 import roslib; roslib.load_manifest('husky_pursuit')
 import rospy, math
-# from std_msgs.msg import Bool
+from std_msgs.msg import Header
 from geometry_msgs.msg import Pose2D
 from nav_msgs.msg import Odometry
-# from sensor_msgs.msg import LaserScan
 
 odom_pub = None
+head = None
+odom = None
 
 def on_pose(pose):
-	odom = Odometry()
+	global odom, head
+	head.stamp = rospy.Time.now()
+
+	odom.header = head
+
 	odom.pose.pose.position.x = pose.x
 	odom.pose.pose.position.y = pose.y
 	odom.pose.pose.orientation.w = math.cos(pose.theta / 2)
 
-	o = 10**-9 # zero
+	odom_pub.publish(odom)
 
+def main():
+	rospy.init_node("pose2d_to_odometry")
+
+	own_num = rospy.get_param('~own_num', 0)
+	
+	global odom, head
+	head = Header()
+	head.stamp = rospy.Time.now()
+	head.frame_id = "robot_{}/odom_combined".format(own_num)
+
+	odom = Odometry()
+	odom.child_frame_id = "robot_{}/base_footprint".format(own_num)
+	o = 10**-9 # zero
 	odom.pose.covariance = [1, 0, 0, 0, 0, 0,
 							0, 1, 0, 0, 0, 0,
 							0, 0, o, 0, 0, 0, # z doesn't change
 							0, 0, 0, o, 0, 0, # constant roll
 							0, 0, 0, 0, o, 0, # constant pitch
 							0, 0, 0, 0, 0, 0.3] # guess - .3 radians rotation cov
-
 	odom.twist.covariance = [99999, 0, 0, 0, 0, 0,
 							 0, 99999, 0, 0, 0, 0,
 							 0, 0, 99999, 0, 0, 0, # large covariances - no data
@@ -36,11 +53,8 @@ def on_pose(pose):
 							 0, 0, 0, 0, 99999, 0,
 							 0, 0, 0, 0, 0, 99999]
 
-	odom_pub.publish(odom)
 
-def main():
-	rospy.init_node("pose2d_to_odometry")
-	
+
 	global odom_pub
 	odom_topic = rospy.get_param('~odometry_publish_topic', 'vo')
 	pose2D_topic = rospy.get_param('~pose2D_topic', 'pose2D')
