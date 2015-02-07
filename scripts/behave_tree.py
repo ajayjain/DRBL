bf415a5d5758471d98bc018cfbe41d384cccaf65
Ajay Jain <ajayjain318@gmail.com>
2015-02-07 13:40:21 -0800

    Infrastructure work on behave_tree

--- scripts/behave_tree.py
+++ scripts/behave_tree.py
@@ -20,7 +20,7 @@
 					while location unknown:
 						validate_and_publish(Wander)
 					return SUCCESS (when location known)
-				Task: PursueWhileAdvisable	(RUNNING while advisable,
+				Task: ChaseWhileAdvisable	(RUNNING while advisable,
 											 SUCCESS when unadvisable)
 					while pursuit is advisable:
 						validate_and_publish(Pursue)
@@ -35,69 +35,152 @@
 			Task: Shooter/Shoot	(RUNNING until a shot is fired, then SUCCESS restarts Behave loop)
 """
 
-import rospy, math
+import rospy, math, random
 import geometry_msgs.msg
+
+from husky_pursuit.msg import RelativePosition
+from pure_seek import seek
+
 from pi_trees_lib.pi_trees_lib import *
 from pi_trees_ros.pi_trees_ros import *
+
+# Parameters for the robot itself
+class Me():
+	MAX_LIN = rospy.get_param('~linear_vel_max',  0.8)
+	MAX_ANG = rospy.get_param('~angular_vel_max', math.pi/2)
 
 class MotionValidator():
 	@classmethod
 	def setup(cls):
-		cls.MAX_LIN = rospy.get_param('~linear_vel_max',  0.8)
-		cls.MAX_ANG = rospy.get_param('~angular_vel_max', math.pi/2)
 		cls.vel_pub = rospy.Publisher('/cmd_vel', geometry_msgs.msg.Twist, latch=True) # remap this
 
 	@classmethod
 	def will_hit_obstacle(cls, vel):
+		#TODO Forecast trajectory from velocity and compare to depth data or vision
 		return False
 
 	@classmethod
 	def validate_and_publish(cls, vel):
 		if cls.will_hit_obstacle(vel):
 			new_vel = geometry_msgs.msg.Twist()
-			new_vel.angular = math.copysign(cls.MAX_ANG, vel.angular) # if angular is near 0, the resulting turn is ~arbitrary
+			new_vel.angular = math.copysign(Me.MAX_ANG, vel.angular) # if angular is near 0, the resulting turn is ~arbitrary
 		else
 			vel_pub.publish(vel)
 
 class BehaviorCoordinator():
 	def __init__(self):
+		# The root node
 		BEHAVE = Sequence("behave")
+		# Track 1 target
+		TARGET_TRACKER = TargetTracker(1)
 
-		TARGET_COUNTER = TargetCounter(1) # destroy 1 target
-		# PARALLEL_MOVE_SHOOT = Parallel
+		# Composite task: returns SUCCESS as soon as any subtask returns SUCCESS
+		PARALLEL_MOVE_SHOOT = ParallelOne("Moving and Shooting in Parallel")
+
+		RESPOND_TO_TARGET = Sequence("respond to target")
+
+		# Behavior task instances
+		WANDER = WanderWhileTargetLocationUnknown("wander to find target", TARGET_TRACKER)
+		# CHASE_TARGET = ChaseWhileAdvisable("chase target", TARGET_TRACKER)
+		# AVOID_TARGET = AvoidTargetWhileAdvisable("avoid target", TARGET_TRACKER)
+
+		RESPOND_TO_TARGET.add_child(WANDER)
+		RESPOND_TO_TARGET.add_child(CHASE_TARGET)
+		RESPOND_TO_TARGET.add_child(AVOID_TARGET)
+
+		PARALLEL_MOVE_SHOOT.add_child(RESPOND_TO_TARGET)
+
+		BEHAVE.add_child(PARALLEL_MOVE_SHOOT)
+
+		# Print a simple representation of the tree
+		print "Behavior Tree Structure"
+		print_tree(BEHAVE)
 
 		# Run the tree
-		while TARGET_COUNTER.is_target_remaining():
+		while TARGET_TRACKER.is_target_remaining():
 			BEHAVE.announce()
 			status = BEHAVE.run()
 			if status == TaskStatus.SUCCESS:
 				print "Finished running tree. All targets destroyed."
 				break
 
-class TargetCounter():
+class TargetTracker():
 	def __init__(self, num_to_destroy):
 		self.num_destroyed = 0
-		self.num_to_destroy = num_to_destroy
-		print "Creating TargetCounter, num_to_destroy=", self.num_to_destroy
+		self.targets = [Target() for i in xrange(num_to_destroy)]
+		print "Created TargetTracker, num_to_destroy=", self.num_to_destroy
 
 	def is_target_remaining(self):
 		#TODO: Actually check if we have destroyed anything, and update the counter
-		if self.num_destroyed < num_to_destroy:
+		if self.num_destroyed < len(targets):
 			return True
 		return False
 
-class IsTargetLocationKnown(MonitorTask):
-	def __init__(self, name, num_to_destroy, *args, **kwargs):
-		super(IsTargetRemaining, self).__init__(name, *args, **kwargs)
-	
+	def choose_first_target(self):
+		if len(self.targets) > 0:
+			for target in self.targets:
+				#TODO: Choose eg closest target
+				if target.is_location_known():
+					return target # chooses the first target with a known location
+		return None
+
+class Target():
+	def __init__(self, health=2 topic='/target_relative', timeout=2): # 2 second timeout
+		self.health = health
+		self.time_last_found = None
+		self.timeout = timeout
+		self.rtheta = None
+		rospy.Subscriber(topic, RelativePosition, self.on_relative) # remap this in the launch file
+
+	def on_relative(self, rel_pos):
+		self.time_last_found = rospy.get_time()
+		self.rtheta = [rel_pos.range, rel_pos.bearing]
+
+	def is_location_known(self):
+		now = rospy.get_time()
+		if self.rtheta != None && self.time_last_found + timeout >= now:
+			return True
+		return False
+
+class WanderWhileTargetLocationUnknown(Task):
+	CMD_FREQ  = 10.0
+	SECONDS_PER_TRANSLATION = 2
+	LOOP_ON_ITER = SECONDS_PER_TRANSLATION * CMD_FREQ
+
+	def __init__(self, name, target_tracker, *args, **kwargs):
+		super(WanderWhileTargetLocationUnknown, self).__init__(name, *args, **kwargs)
+		
+		self.name = name
+		self.target_tracker = target_tracker
+
+		self.count = LOOP_ON_ITER
+		self.trans = None
+
+		print "Created task WanderWhileTargetLocationUnknown"
+
+	def random_translation():
+		x = random.uniform(-10, 10)
+		y = random.uniform(-10, 10)
+		return (x, y, 0)
 
 	def run(self):
+		while target_tracker.choose_first_target() == None:
+			if self.count == LOOP_ON_ITER:
+				trans = random_translation()
+				print "Seeking trans", trans
+				self.count = 0
+			self.count += 1
+
+			# Seek the random translation and send off
+			vel = seek(trans, Me.MAX_LIN, Me.MAX_ANG)
+			MotionValidator.validate_and_publish(vel)
+
+		return TaskStatus.SUCCESS
 
 if __name__ == "__main__":
 	rospy.init_node("behave_tree")
 	
 	MotionValidator.setup()
 	BehaviorCoordinator()
-	# rospy.Subscriber('/target_relative', RelativePosition, on_relative) # remap this in the launch file
 
 	rospy.spin()
